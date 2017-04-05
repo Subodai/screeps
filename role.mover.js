807dfd05f76842d20605e4f1aec98489398730e4
Mark Thompson <subodai85@gmail.com>
2017-04-05 09:18:26 +0100

    Building up mover logic
    Started work on the logic for finding pickup targets
    Added memory for targetType
    Added some basic docs for creep memory makeup

--- role.mover.js
+++ role.mover.js
@@ -39,6 +39,15 @@ module.exports.roster = {
     XL: 5   // 5 XL
 };
 
+/**
+ * @memory.tartgetId    int     The current id of the oject this creep is interested in
+ * @memory.targetType   string  The type of target this creep is interested in
+ * @memory.dying        bool    If the creep has less than 100 ticks to live
+ * @memory.delivering   bool    If the creep is currently in drop-off mode
+ * @memory.moving       bool    If the creep cannot perform an action it will be in moving mode
+ * @memory.path         string  Serialised path for the creep to get to it's target
+
+
 /* Run method */
 module.exports.run = function (creep, debug = false) {
     // If we're fatigued, stop and go no further
@@ -62,6 +71,7 @@ module.exports.run = function (creep, debug = false) {
     // Is it full and NOT delivering
     if (!creep.memory.delivering && creep.carry.energy == creep.carryCapacity) {
         delete creep.memory.targetId;
+        delete creep.memory.targetType;
         delete creep.memory.path;
         creep.memory.delivering = true;
     }
@@ -70,12 +80,19 @@ module.exports.run = function (creep, debug = false) {
 
     // Are we in emergency mode?
     if (creep.room.memory.emergency) {
-
+        // If we are not deliverying
+        if (!creep.memory.delivering) {
+            // Make sure our targetId is the room storage
+            if (creep.memory.targetId != creep.room.storage.id) {
+                creep.memory.targetId = creep.room.storage.id;
+                creep.memory.targetType = 'storage';
+            }
+        }
     }
 
     // Are we in guard mode?
     if (creep.room.memory.guard) {
-
+        // We'll get to this later
     }
 
     // Override 2
@@ -86,7 +103,54 @@ module.exports.run = function (creep, debug = false) {
     if (creep.memory.delivering) {
         // Try dropping off
     } else {
-        // Try collecting
-    }
+        // Do we have a target?
+        if (!creep.memory.targetId) {
+            // No, lets find one
+            // The priority is dropped resources
+            var resource = creep.pos.findClosestyRange(FIND_DROPPED_RESOURCES, {
+                filter: (i) => i.amount >= creep.carryCapacity/2
+            });
+            // Did we find resources?
+            if (resource) {
+                creep.memory.targetId = resource.id;
+                creep.memory.targetType = 'resource';
+            } else {
+                // okay we didn't find suitable dropped res, next lets try a container
+                var container = creep.pos.findClosestyRange(FIND_STRUCTURES, {
+                    filter: (i) => i.structureType == STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] >= 100
+                });
+                // Did we find a container?
+                if (container) {
+                    creep.memory.targetId = container.id;
+                    creep.memory.targetType = 'container';
+                } else {
+                    // TODO Some kind of "Oh shit I didn't find a suitable target" code here
+                }
+            }
+        }
 
+        var target = Game.getObjectById(creep.memory.targetId);
+
+        // Resource Type
+        if (creep.memory.targetType == 'resource') {
+            // Let's try to pick it up
+            if (creep.pickup(target) == ERR_NOT_IN_RANGE) {
+                // We need to go get it
+                creep.memory.moving = true;
+            } else {
+                // Pickup successful
+            }
+        }
+
+        // Storage or Container Type
+        if (creep.memory.targetType == 'storage' || creep.memory.targetType == 'container') {
+            // Try to withdraw
+            if (creep.withdraw(box, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                // We need to go to it
+                creep.memory.moving = true;
+            } else {
+                // Pickup successful
+            }
+        }
+    }
 };
