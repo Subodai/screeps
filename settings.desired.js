e8efc1757c8e164511b9c3da9af9fb9d839422e7
Mark Thompson <subodai85@gmail.com>
2017-04-20 11:13:28 +0100

    Turn upgraders on

--- settings.desired.js
+++ settings.desired.js
@@ -15,30 +15,10 @@ module.exports = {
     SpawnExtractors: false,
     SpawnHarvesters: true,
     SpawnBuilders: true,
+    SpawnUpgraders: true,
 
     SpawnMovers: false,
     SpawnScouts: false,
 
     emptyContainers: false,
-
-    SpawnBig: true,
-    SpawnMid: true,
-    SpawnSml: true,
-
-
-
-    // OLD DESIRED CODE
-    // Desired Small sized creeps
-    SmlH: 0,
-    SmlU: 0,
-    SmlB: 0,
-    SmlR: 0,
-    // Desired Mid sized creeps
-    MidH: 0,
-    MidU: 0,
-    MidB: 0,
-    // Desired BIG sized creeps
-    BigH: 0,
-    BigU: 0,
-    BigB: 0
 }
