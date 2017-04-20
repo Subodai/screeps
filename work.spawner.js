9c394951b48fdf369c909d15099e53893879ce9e
Mark Thompson <subodai85@gmail.com>
2017-04-20 21:02:52 +0100

    Remove big mid and small spawners

--- work.spawner.js
+++ work.spawner.js
@@ -1,9 +1,6 @@
 /*
  * The almighty Spawner!
  */
-var smallCost = 300;
-var midCost = 450;
-var bigCost = 650;
 
 var miner = require('role.miner');
 var minerSpawn = require('spawn.miner');
@@ -93,29 +90,7 @@ var spawner = {
                     return;
                 }
             }
-            // if (desired.SpawnBig && Game.rooms[name].energyAvailable >= bigCost) {
-            //     if (debug) { console.log('Enough Energy for Big Spawn'); }
-            //     var bigSpawner = require('spawn.big');
-            //     if (bigSpawner.run(debug)) {
-            //         console.log('Spawner used ' + (Game.cpu.getUsed() - _cpu).toFixed(3) + ' CPU');
-            //         return;
-            //     }
-            // }
-            // if (desired.SpawnMid && Game.rooms[name].energyAvailable >= midCost) {
-            //     if (debug) { console.log('Enough Energy for Mid Spawn'); }
-            //     var midSpawner = require('spawn.mid');
-            //     if (midSpawner.run(debug)) {
-            //         console.log('Spawner used ' + (Game.cpu.getUsed() - _cpu).toFixed(3) + ' CPU');
-            //         return;
-            //     }
-            // }
-            // if (desired.SpawnSml && Game.rooms[name].energyAvailable >= smallCost) {
-            //     if (debug) { console.log('Enough Energy for Small Spawn'); }
-            //     var smallspawner = require('spawn.small');
-            //     smallspawner.run(debug);
-            //     console.log('Spawner used ' + (Game.cpu.getUsed() - _cpu).toFixed(3) + ' CPU');
-            //     return;
-            // }
+            
             if (debug) { console.log('Not Enough Energy for Active Spawns'); }
         }
 
