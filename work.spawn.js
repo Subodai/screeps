94821ee511b1c549b0acec9769fb8027add0a8a1
Mark Thompson <subodai85@gmail.com>
2017-04-21 22:39:41 +0100

    fix logging

--- work.spawn.js
+++ work.spawn.js
@@ -33,10 +33,10 @@ module.exports.run = function(debug = false) {
                 console.log('role ' + role + ' has requirent' + _role.roomRequirement);
                 // if it requires minersNeeded do some things
                 if (_room.memory[_role.roomRequirement] && _room.memory[_role.roomRequirement] > 0) {
-                    console.log('room ' + room + ' requires ' + _room.memory[_role.roomRequirement]);
+                    console.log('room ' + _room + ' requires ' + _room.memory[_role.roomRequirement]);
                     // We need the rooms miner count
                     var list = _.filter(Game.creeps, (creep) => creep.memory.role == _role.roleName && creep.room == _room && !creep.memory.dying);
-                    console.log('room ' + room + ' has ' + list.length);
+                    console.log('room ' + _room + ' has ' + list.length);
                     if (list.length >= _room.memory[_role.roomRequirement]) {
                         continue;
                     }
