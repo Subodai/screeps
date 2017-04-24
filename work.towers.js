9d817c46ec71004ea58556f8cc32ed8548ca4c79
Mark Thompson <subodai85@gmail.com>
2017-04-24 10:54:21 +0100

    Comments

--- work.towers.js
+++ work.towers.js
@@ -3,8 +3,9 @@ var towerRunner = require('role.tower');
  * Tower Handler
  */
 module.exports.run = function (debug = false) {
-
+    // Loop through all spawns
     for (var spawn in Game.spawns) {
+        // Make spawn object
         var _spawn = Game.spawns[spawn];
         // Find our towers
         var towers = _spawn.room.find(FIND_MY_STRUCTURES, {
@@ -12,7 +13,7 @@ module.exports.run = function (debug = false) {
         });
         // Do we even have any?
         if(towers.length > 0) {
-            // Loop
+            // Loop through them all
             for (var i=0;i<=towers.length-1;i++) {
                 var tower = towers[i];
                 towerRunner.run(tower, debug);
