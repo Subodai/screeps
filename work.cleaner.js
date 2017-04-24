42d76a582e1c298c0bacc3ce9da0db5cb676a52b
Mark Thompson <subodai85@gmail.com>
2017-04-24 13:20:30 +0100

    Make the cleaner a bit less spammy

--- work.cleaner.js
+++ work.cleaner.js
@@ -1,13 +1,13 @@
 /*
  * Memory Cleaner
  */
-module.exports.run = function() {
+module.exports.run = function(debug = false) {
     var _cpu = Game.cpu.getUsed();
     for(var name in Memory.creeps) {
         if(!Game.creeps[name]) {
             delete Memory.creeps[name];
-            console.log('Memory cleared[', name,']');
+            console.log('Memory cleared [' + name +']');
         }
     }
-    console.log('Cleaner used ' + (Game.cpu.getUsed() - _cpu).toFixed(3) + ' CPU');
+    if (debug) { console.log('Cleaner used ' + (Game.cpu.getUsed() - _cpu).toFixed(3) + ' CPU'); }
 }
