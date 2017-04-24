6c226eb422167d5ee50a3fdcc06daa224ac33747
Mark Thompson <subodai85@gmail.com>
2017-04-24 10:52:04 +0100

    Fix towers in multiple rooms

--- work.towers.js
+++ work.towers.js
@@ -3,16 +3,20 @@ var towerRunner = require('role.tower');
  * Tower Handler
  */
 module.exports.run = function (debug = false) {
-    // Find our towers
-    var towers = Game.spawns.Sub1.room.find(FIND_MY_STRUCTURES, {
-        filter: (i) => i.structureType == STRUCTURE_TOWER && i.energy > 0
-    });
-    // Do we even have any?
-    if(towers.length > 0) {
-        // Loop
-        for (var i=0;i<=towers.length-1;i++) {
-            var tower = towers[i];
-            towerRunner.run(tower, debug);
+
+    for (var spawn in Game.spawns) {
+        var _spawn = Game.spawns[spawn];
+        // Find our towers
+        var towers = _spawn.room.find(FIND_MY_STRUCTURES, {
+            filter: (i) => i.structureType == STRUCTURE_TOWER && i.energy > 0
+        });
+        // Do we even have any?
+        if(towers.length > 0) {
+            // Loop
+            for (var i=0;i<=towers.length-1;i++) {
+                var tower = towers[i];
+                towerRunner.run(tower, debug);
+            }
         }
     }
 }
