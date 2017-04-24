f56f3ef43a0ea7109b3c93a7e60b1f221662ceab
Mark Thompson <subodai85@gmail.com>
2017-04-24 16:21:39 +0100

    Reminder for market deal command

--- commands.js
+++ commands.js
@@ -2,3 +2,4 @@
 Game.spawns['Spawn1'].room.createConstructionSite( 23, 22, STRUCTURE_TOWER );
 // Safemode
 Game.spawns['Spawn1'].room.controller.activateSafeMode();
+Game.market.deal('58fe154d320b7ca308d38d42',32,'W84S73');
