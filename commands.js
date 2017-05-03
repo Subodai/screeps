932dd03dc14dd657942ffadff02dec5629c866da
Mark Thompson <mark@artsgraphica.com>
2017-05-03 21:21:41 +0100

    Update the commands list

--- commands.js
+++ commands.js
@@ -4,6 +4,6 @@ Game.spawns['Spawn1'].room.createConstructionSite( 23, 22, STRUCTURE_TOWER );
 Game.spawns['Spawn1'].room.controller.activateSafeMode();
 Game.market.deal('58fe154d320b7ca308d38d42',32,'W84S73');
 
-global.napList = ['sub'];
-console.log('test ' + !(global.napList.indexOf('test') > -1));
-console.log('sub ' + !(global.napList.indexOf('sub') > -1));
+global.friends = ['sub'];
+console.log('test ' + !(global.friends.indexOf('test') > -1));
+console.log('sub ' + !(global.friends.indexOf('sub') > -1));
