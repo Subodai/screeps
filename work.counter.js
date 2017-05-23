b12ebbef87bf40dfab4ecf29fe1f8a7e7617f17a
Mark Thompson <subodai85@gmail.com>
2017-05-23 08:47:55 +0100

    Counter tweaks

--- work.counter.js
+++ work.counter.js
@@ -10,7 +10,7 @@ module.exports.run = function(debug = false) {
     // Loop through the rooms
     for(var room in Game.rooms) {
         var theRoom = Game.rooms[room];
-        if (!theRoom) { continue; }
+        if (theRoom == undefined) { continue; }
         var miners = spawner.count('miner', room);
         var extractors = spawner.count('extractor', room);
         // Are there any hostiles?
@@ -32,13 +32,13 @@ module.exports.run = function(debug = false) {
             console.log('We are still in guard mode');
         }
         var list = _.filter(Game.creeps, (creep) => !creep.memory.dying && creep.memory.roomName == room);
-        if ((list.length == 0 && miners == 0) && !theRoom.memory.emergency){
+        if ((list.length <= 2 && miners == 0) && !theRoom.memory.emergency){
             notify = true;
             msg += "\n" + Game.time + ' Room '+ room + ' In Emergency Mode!!' + "\n";
             console.log('Emergency Activated');
             theRoom.memory.emergency = true;
         }
-        if ((list.length >= 5 || miners > 0) && theRoom.memory.emergency) {
+        if ((list.length >= 10 || miners > 0) && theRoom.memory.emergency) {
             notify = true;
             msg += "\n" + Game.time + ' Room ' + room + ' No Longer in Emergency Mode' + "\n";
             console.log('Emergency Deactivated');
@@ -46,7 +46,7 @@ module.exports.run = function(debug = false) {
             delete theRoom.memory.emergency;
         }
         if (theRoom.memory.emergency) {
-            console.log('We have ' + list.length + ' total creeps still in emergency mode');
+            console.log(room + ' has ' + list.length + ' total creeps still in emergency mode');
         }
 
         msg += ' [' + room + ':' + theRoom.energyAvailable + ']';
