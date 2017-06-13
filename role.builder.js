257cc4bd8611448d124296d7533a584cff7e361d
Mark Thompson <subodai85@gmail.com>
2017-06-13 13:57:33 +0100

    Stop builder spam!

--- role.builder.js
+++ role.builder.js
@@ -59,12 +59,12 @@ module.exports.body = {
 module.exports.roster = {
     1: 3,
     2: 3,
-    3: 1,
-    4: 1,
-    5: 3,
-    6: 3,
-    7: 3,
-    8: 1,
+    3: 2,
+    4: 2,
+    5: 2,
+    6: 2,
+    7: 2,
+    8: 2,
 }
 
 module.exports.enabled = function (room, debug = false) {
