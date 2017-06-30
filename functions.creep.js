4a8f9daf7facd0a6a96e4371bcf24c86d589a56d
Mark Thompson <subodai85@gmail.com>
2017-06-30 12:13:39 +0100

    Added a bunch of debugging to Creep extension file

--- functions.creep.js
+++ functions.creep.js
@@ -1,15 +1,23 @@
 
+// Change to true to enable debugging
+const DBG = false;
 /**
- * Bunch of Handy Creep Functions
+ * Find and collect nearby energy
+ *
+ * @param useStorage bool
+ * @param emergency bool
+ * @param options object
  */
 Creep.prototype.getNearbyEnergy = function(useStorage = false, emergency = false, options = {}) {
     // First, are we full?
     if (_.sum(this.carry) == this.carryCapacity) {
+        DGB && console.log('Creep Full Cannot Get Nearby Energy');
         // Clear our pickup target
         delete this.memory.energyPickup;
         return ERR_FULL;
     }
     if (!this.memory.energyPickup) {
+        DGB && console.log('Creep has no memory, finding stuff to pickup');
         // If this is an emergency we should be going for the terminal, then storage
         if (emergency) {
             // TODO EMPTY TERMINAL AND STORAGE HERE PLEASE
@@ -24,9 +32,10 @@ Creep.prototype.getNearbyEnergy = function(useStorage = false, emergency = false
         });
         // False some things
         var resource = container = false;
-        var thisCreep = this;
+        const thisCreep = this;
         // If we have resources
         if (resources.length > 0) {
+            DGB && console.log('Found ' + resources.length + ' resource piles');
             // Sort the resources
             resources.sort(function(a,b) {
                 return thisCreep.pos.getRangeTo(a) - thisCreep.pos.getRangeTo(b);
@@ -36,6 +45,7 @@ Creep.prototype.getNearbyEnergy = function(useStorage = false, emergency = false
         }
         // if we have containers
         if (containers.length > 0) {
+            DGB && console.log('Found ' + containers.length + ' containers');
             // Sort the containers
             containers.sort(function(a,b) {
                 return thisCreep.pos.getRangeTo(a) - thisCreep.pos.getRangeTo(b);
@@ -46,18 +56,23 @@ Creep.prototype.getNearbyEnergy = function(useStorage = false, emergency = false
         if (resource && container) {
             // If the resource is closer
             if (this.pos.getRangeTo(resource) < this.pos.getRangeTo(container)) {
+                DGB && console.log('Stored resource pile ' + resource.id + ' in creep memory');
                 this.memory.energyPickup = resource.id;
             } else {
+                DGB && console.log('Stored container ' + container.id + ' in creep memory');
                 this.memory.energyPickup = container.id;
             }
         } else if (resource) {
+            DGB && console.log('Stored resource pile ' + resource.id + ' in creep memory');
             this.memory.energyPickup = resource.id;
         } else if (container) {
+            DGB && console.log('Stored container ' + container.id + ' in creep memory');
             this.memory.energyPickup = container.id;
         }
     }
     // Do we have a target?
     if (this.memory.energyPickup) {
+        DGB && console.log('Found Energy source in creeps memeory ' + this.memory.energyPickup);
         // We do! let's grab it
         const target = Game.getObjectById(this.memory.energyPickup);
         if (options == {}) {
@@ -72,44 +87,76 @@ Creep.prototype.getNearbyEnergy = function(useStorage = false, emergency = false
         var pickupSuccess = true;
         // Alright what is it?
         if (target instanceof Resource) { // Resource
+            DGB && console.log('Target is a Resource');
             // Is there still enough of it?
-            if (target.amount < this.carryCapacity) {
+            if (target.amount < (this.carryCapacity - _.sum(this.carry))) {
+                DGB && console.log('Resource no longer viable clearing memory');
                 // Target has gone, clear memory
                 delete this.memory.energyPickup;
                 return ERR_INVALID_TARGET;
             }
-            // First attempt to pickitup
-            if (this.pickup(target) == ERR_NOT_IN_RANGE) {
+            // Only bother trying to pick up if we're within 1 range
+            if (this.getRangeTo(target) <= 1) {
+                DGB && console.log('Target should be in range, attempting pickup');
+                // First attempt to pickitup
+                if (this.pickup(target) == ERR_NOT_IN_RANGE) {
+                    DGB && console.log('Pickup failed');
+                    var pickupSuccess = false;
+                }
+            } else {
+                DGB && console.log('Target not in range');
                 var pickupSuccess = false;
             }
-        } else if (target instanceof StructureContainer || target instanceof StructureStorage) { // Container
+
+        } else if (target instanceof StructureContainer || target instanceof StructureStorage || target instanceof StructureTerminal) { // Container
+            DGB && console.log('Target is a Container, Storage, or Terminal');
             // Check the container still has the energy
-            if (target.store[RESOURCE_ENERGY] < this.carryCapacity) {
+            if (target.store[RESOURCE_ENERGY] < (this.carryCapacity - _.sum(this.carry))) {
+                DGB && console.log('Target no longer has enough energy clearing memory');
                 // Clear memory and return invalid target
                 delete this.memory.energyPickup;
                 return ERR_INVALID_TARGET;
             }
-            // Lets attempt to withdraw
-            if (this.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            // Only bother trying to pick up if we're within 1 range
+            if (this.getRangeTo(target) <= 1) {
+                DGB && console.log('Target should be in range, attempting withdraw');
+                // Lets attempt to withdraw
+                if (this.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    DGB && console.log('Withdraw failed');
+                    var pickupSuccess = false;
+                }
+            } else {
+                DGB && console.log('Target not in range');
                 var pickupSuccess = false;
             }
         } else if (target instanceof Source) { // Source
             // Does it still have energy ?
             if (target.energy == 0) {
+                DGB && console.log('Source no longer has energy, clearing memory');
                 // no clear the memory
                 delete this.memory.energyPickup;
                 return ERR_INVALID_TARGET;
             }
-            // Alright lets try harvesting it
-            if (this.harvest(source) == ERR_NOT_IN_RANGE) {
+            // Only bother trying to pick up if we're within 1 range
+            if (this.getRangeTo(target) <= 1) {
+                DGB && console.log('Target should be in range, attempting harvest');
+                // Alright lets try harvesting it
+                if (this.harvest(source) == ERR_NOT_IN_RANGE) {
+                    DGB && console.log('Harvest failed');
+                    var pickupSuccess = false;
+                }
+            } else {
+                DGB && console.log('Target not in range');
                 var pickupSuccess = false;
             }
         }
         // Did we successfully pick up the thing
         if (pickupSuccess) {
+            DGB && console.log('Successfully gathered resources');
             this.say(global.sayWithdraw);
             // Are we now full?
             if (this.carry.energy == this.carryCapacity) {
+                DGB && console.log('Creep is now full clearing pickup memory');
                 // Alright we're full clear memory and return full
                 delete this.memory.energyPickup;
                 return ERR_FULL;
@@ -117,6 +164,7 @@ Creep.prototype.getNearbyEnergy = function(useStorage = false, emergency = false
             // Just return OK, we're not full yet
             return OK;
         } else {
+            DGB && console.log('Moving closer to target');
             // We probably need to move
             this.moveTo(target, options);
             // Say!
