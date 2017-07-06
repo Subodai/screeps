f2b904007b8f55367dbafe24ef5c72d3688abc3e
Mark Thompson <subodai85@gmail.com>
2017-07-06 22:59:37 +0100

    Some nice visuals for tower ranges

--- work.towers.js
+++ work.towers.js
@@ -30,4 +30,30 @@ module.exports.colour = function (tower, cost) {
         align:'center',
         stroke:'rgba(0,0,0,0.5)',
     });
+    /* Remove comment to show tower coverage
+    .rect(tower.pos.x - 20, tower.pos.y - 20, 40, 40, {
+        stroke: 'white',
+        lineStyle: 'dashed',
+        opacity: 0.01,
+        fill: color,
+        strokeWidth: 0.2
+    }).rect(tower.pos.x - 15, tower.pos.y - 15, 30, 30, {
+        stroke: 'white',
+        lineStyle: 'dashed',
+        opacity: 0.01,
+        fill: color,
+        strokeWidth: 0.2
+    }).rect(tower.pos.x - 10, tower.pos.y - 10, 20, 20, {
+        stroke: 'white',
+        lineStyle: 'dashed',
+        opacity: 0.01,
+        fill: color,
+        strokeWidth: 0.2
+    }).rect(tower.pos.x - 5, tower.pos.y - 5, 10, 10, {
+        stroke: 'white',
+        strokeWidth: 0.2,
+        lineStyle: 'dashed',
+        opacity: 0.01,
+        fill: color
+    });*/
 }
