9b4ff3cf85054885bfe0b5ebc155ebcc412ff3c9
Mark Thompson <subodai85@gmail.com>
2017-07-08 11:10:57 +0100

    Formatting

--- main.js
+++ main.js
@@ -17,13 +17,12 @@ var protoypes = [
 //     protoypes[i]();
 // }
 
-
-var spawner   = require('work.spawn');
-var movement  = require('work.movement');
-var cleaner   = require('work.cleaner');
-var towers    = require('work.towers');
-var counter   = require('work.counter');
-var screepsplus     = require('screepsplus');
+var spawner     = require('work.spawn');
+var movement    = require('work.movement');
+var cleaner     = require('work.cleaner');
+var towers      = require('work.towers');
+var counter     = require('work.counter');
+var screepsplus = require('screepsplus');
 
 // Load the new Queue
 const q = require('prototype.queue');
