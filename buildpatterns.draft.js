a67827dc25f9c159f9cfb2fe8e383a38f764d755
Mark Thompson <subodai85@gmail.com>
2017-08-13 17:44:44 +0100

    Buildpattern draft file

---
+++ buildpatterns.draft.js
@@ -0,0 +1,53 @@
+
+// Build Patterns
+
+RoomPosition.prototype.seedFlags = function() {
+    let spawns = [
+        [0,0]
+    ];
+
+    let roads = [
+        // Outer Circle
+        [-1,-1],[ 0,-1],[ 1,-1],[-1, 0],[ 1, 0],[-1, 1],[ 0, 1],[ 1, 1],
+        // Spoke 1
+        [2,-2],[3,-3],
+        // Spoke 2
+        [-2,-2],[-3,-3],
+        // Spoke 3
+        [-2,2],[-3,3],
+        // Spoke 4
+        [2,2],[3,3],
+    ];
+
+    let extensions = [
+        // Spoke 1
+        [2,-1],[3,-1],[3,-2],[4,-2],[4,-3],[1,-2],[1,-3],[2,-3],[2,-4],[3,-4],
+        // Spoke 2
+        [-2,-1],[-3,-1],[-3,-2],[-4,-2],[-4,-3],[-1,-2],[-1,-3],[-2,-3],[-2,-4],[-3,-4],
+        // Spoke 3
+        [-2,1],[-3,1],[-3,2],[-4,2],[-4,3],[-1,2],[-1,3],[-2,3],[-2,4],[-3,4],
+        // Spoke 4
+        [2,1],[3,1],[3,2],[4,2],[4,3],[1,2],[1,3],[2,3],[2,4],[3,4],
+    ];
+
+    for (let coords of spawns) {
+        let dX = coords[0];
+        let dY = coords[1];
+        let newPos = new RoomPosition(this.x+dX, this.y+dY,this.roomName);
+        newPos.createFlag(null,COLOR_WHITE,COLOR_GREY);
+    }
+
+    for (let coords of extensions) {
+        let dX = coords[0];
+        let dY = coords[1];
+        let newPos = new RoomPosition(this.x+dX, this.y+dY,this.roomName);
+        newPos.createFlag(null,COLOR_WHITE,COLOR_YELLOW);
+    }
+
+    for (let coords of roads) {
+        let dX = coords[0];
+        let dY = coords[1];
+        let newPos = new RoomPosition(this.x+dX, this.y+dY,this.roomName);
+        newPos.createFlag(null,COLOR_WHITE,COLOR_WHITE);
+    }
+}
