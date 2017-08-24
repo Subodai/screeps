17f51e655f0b6891cdaa4d014fad9c307bad867a
Mark Thompson <subodai85@gmail.com>
2017-08-24 14:01:23 +0100

    Moar improvements!

--- role.janitor.js
+++ role.janitor.js
@@ -137,13 +137,13 @@ module.exports.run = function(creep) {
     // If we have only a few ticks to live we should set to dying
     if (creep.ticksToLive < 100) { creep.memory.dying = true; }
     // If we're sapping and have run out of energy
-    if(creep.memory.sapping && creep.carry.energy == 0) {
+    if(creep.memory.sapping && creep.carry.energy === 0) {
         // clear sapping flag
         creep.memory.sapping = false;
         creep.say(global.sayGet);
     }
     // Are we full?
-    if(!creep.memory.sapping && creep.carryCapacity > 0 && creep.carry.energy == creep.carryCapacity) {
+    if(!creep.memory.sapping && creep.carryCapacity > 0 && creep.carry.energy === creep.carryCapacity) {
         // Yes set sapping flag to true
         creep.memory.sapping = true;
         creep.say(global.sayPUT);
